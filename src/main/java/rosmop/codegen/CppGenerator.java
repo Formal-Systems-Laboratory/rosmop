33e2fbc779c7c897fca7b17025754b769c2dd52b
Manasvi Saxena <msaxena2@illinois.edu>
2019-07-17 17:43:56 -0700

    code/{Cpp,Header}Generator: Formatting

--- src/main/java/rosmop/codegen/CppGenerator.java
+++ src/main/java/rosmop/codegen/CppGenerator.java
@@ -20,7 +20,7 @@ import com.runtimeverification.rvmonitor.logicpluginshells.LogicPluginShellResul
  * @author Cansu Erdogan
  *
  * Generates rvmonitor.cpp file from the wrapped ASTs of input specification files
- * 
+ *
  */
 public class CppGenerator {
 
@@ -100,7 +100,7 @@ public class CppGenerator {
 			printer.printLn();
 		} else {
 		    toWrite.values().forEach(x -> {
-		        if(x != null) {
+			if(x != null) {
 					printer.print(x.properties.getOrDefault("state setters", "").toString());
 					printer.printLn("");
 
@@ -109,7 +109,7 @@ public class CppGenerator {
 				}
 		    });
 			printMonitorCallbacks(toWrite, true);
-		 	printMain(toWrite);
+			printMain(toWrite);
 		}
 
 		Tool.writeFile(printer.getSource(), outputPath);
@@ -165,7 +165,7 @@ public class CppGenerator {
 		printer.printLn("std::set<std::string> " + GeneratorUtil.MONITOR_TOPICS_VAR + ";");
 		printer.printLn("std::set<std::string> " + GeneratorUtil.MONITOR_TOPICS_ALL + ";");
 		printer.printLn("std::set<std::string> " + GeneratorUtil.MONITOR_TOPICS_ENB + ";");
-		printer.printLn("std::map<std::string,std::string> " 
+		printer.printLn("std::map<std::string,std::string> "
 				+ GeneratorUtil.MONITOR_TOPICS_AND_TYPES + ";");
 
 		printer.printLn();
@@ -175,7 +175,7 @@ public class CppGenerator {
 		printAdvertisingOptions(toWrite);
 
 		printer.unindent();
-		printer.printLn("}");	
+		printer.printLn("}");
 		printer.printLn();
 	}
 
@@ -187,12 +187,12 @@ public class CppGenerator {
 
 		for (String topic : HeaderGenerator.addedTopics.keySet()) {
 			printer.printLn(GeneratorUtil.MONITOR_TOPICS_VAR + ".insert(\"" + topic + "\");");
-			printer.printLn(GeneratorUtil.MONITOR_TOPICS_AND_TYPES + "[\"" + topic + "\"] = \"" 
+			printer.printLn(GeneratorUtil.MONITOR_TOPICS_AND_TYPES + "[\"" + topic + "\"] = \""
 					+ HeaderGenerator.addedTopics.get(topic).get(0).getMsgType() + "\";");
 		}
 		printer.printLn();
 		for (CSpecification rvcParser : toWrite.keySet()) {
-			printer.printLn(GeneratorUtil.MONITOR_TOPICS_ALL + ".insert(\"" 
+			printer.printLn(GeneratorUtil.MONITOR_TOPICS_ALL + ".insert(\""
 					+ rvcParser.getSpecName() + "\");");
 		}
 		printer.printLn();
@@ -203,7 +203,7 @@ public class CppGenerator {
 
 	private static void printAdvertisingOptions(
 			HashMap<CSpecification, LogicPluginShellResult> toWrite) {
-		printer.printLn("void initAdvertiseOptions(std::string topic, ros::AdvertiseOptions" 
+		printer.printLn("void initAdvertiseOptions(std::string topic, ros::AdvertiseOptions"
 			+ " &" + GeneratorUtil.ADVERTISE_OPTIONS + ")");
 		printer.printLn("{");
 		printer.indent();
@@ -224,8 +224,8 @@ public class CppGenerator {
 			printer.printLn("if (topic == \"" + topic + "\") {");
 
 			printer.indent();
-			printer.printLn(GeneratorUtil.ADVERTISE_OPTIONS + ".init<" 
-					+ HeaderGenerator.addedTopics.get(topic).get(0).classifyMsgType() 
+			printer.printLn(GeneratorUtil.ADVERTISE_OPTIONS + ".init<"
+					+ HeaderGenerator.addedTopics.get(topic).get(0).classifyMsgType()
 					+ ">(topic, 1000);");
 			printer.unindent();
 			printer.printLn("}");
@@ -233,16 +233,16 @@ public class CppGenerator {
 
 		printer.unindent();
 		printer.printLn("}");
-		printer.printLn();		
+		printer.printLn();
 	}
 
 	/**
 	 * Generate code for constructor
-	 * @param toWrite 
+	 * @param toWrite
 	 */
 	private static void printConstructor(HashMap<CSpecification, LogicPluginShellResult> toWrite){
-		printer.printLn(GeneratorUtil.MONITOR_CLASS_NAME + "::" + GeneratorUtil.MONITOR_CLASS_NAME 
-				+ "(string topic, ros::SubscribeOptions &" 
+		printer.printLn(GeneratorUtil.MONITOR_CLASS_NAME + "::" + GeneratorUtil.MONITOR_CLASS_NAME
+				+ "(string topic, ros::SubscribeOptions &"
 				+ GeneratorUtil.SUBSCRIBE_OPTIONS + ")");
 		printer.printLn("{");
 		printer.indent();
@@ -250,7 +250,7 @@ public class CppGenerator {
 		//	     topic_name = topic;
 		//	     server_manager = rv::ServerManager::instance(
 		printer.printLn(GeneratorUtil.TOPIC_PTR_NAME + " = topic;");
-		printer.printLn(GeneratorUtil.SERVERMANAGER_PTR_NAME 
+		printer.printLn(GeneratorUtil.SERVERMANAGER_PTR_NAME
 				+ " = rv::ServerManager::instance();");
 
 		if(HeaderGenerator.hasInit){
@@ -275,17 +275,17 @@ public class CppGenerator {
 			printer.indent();
 
 			if(HeaderGenerator.addedTopics.get(topic).size() == 1){
-				printer.printLn(GeneratorUtil.SUBSCRIBE_OPTIONS + ".init<" + 
-						HeaderGenerator.addedTopics.get(topic).get(0).classifyMsgType() 
+				printer.printLn(GeneratorUtil.SUBSCRIBE_OPTIONS + ".init<" +
+						HeaderGenerator.addedTopics.get(topic).get(0).classifyMsgType()
 						+ ">(topic, 1000, boost::bind(&" + GeneratorUtil.MONITOR_CLASS_NAME +
-						"::monitorCallback_" 
-						+ HeaderGenerator.addedTopics.get(topic).get(0).getName() 
+						"::monitorCallback_"
+						+ HeaderGenerator.addedTopics.get(topic).get(0).getName()
 						+ ", this, _1));");
 			}else{
 				String callback = "::mergedMonitorCallback_" + topic.replace("/", "");
 
-				printer.printLn(GeneratorUtil.SUBSCRIBE_OPTIONS + ".init<" + 
-						HeaderGenerator.addedTopics.get(topic).get(0).classifyMsgType() 
+				printer.printLn(GeneratorUtil.SUBSCRIBE_OPTIONS + ".init<" +
+						HeaderGenerator.addedTopics.get(topic).get(0).classifyMsgType()
 						+ ">(topic, 1000, boost::bind(&" + GeneratorUtil.MONITOR_CLASS_NAME +
 						callback + ", this, _1));");
 			}
@@ -323,7 +323,7 @@ public class CppGenerator {
 					printer.printLn();
 
 					for(ROSEvent mergeevents : HeaderGenerator.addedTopics.get(event.getTopic())){
-						if(toWrite.get(rvcParser) == null || 
+						if(toWrite.get(rvcParser) == null ||
 								!rvcParser.getEvents().keySet().contains(mergeevents.getName())){
 							printActionCode(mergeevents, monitorAsNode);
 						}else{
@@ -361,7 +361,7 @@ public class CppGenerator {
 					printParametersBinding(event);
 					printer.printLn();
 
-					if(toWrite.get(rvcParser) == null || 
+					if(toWrite.get(rvcParser) == null ||
 							!rvcParser.getEvents().keySet().contains(event.getName())){
 						printActionCode(event, monitorAsNode);
 					}else{
@@ -382,7 +382,7 @@ public class CppGenerator {
 
 			if(toWrite.get(rvcParser) != null){
 				String str = (String) toWrite.get(rvcParser).properties.get("event functions");
-				printer.printLn(str.replace("void\n__RVC_", "void " 
+				printer.printLn(str.replace("void\n__RVC_", "void "
 						+ GeneratorUtil.MONITOR_CLASS_NAME + "::__RVC_"));
 				printer.printLn();
 			}
@@ -390,7 +390,7 @@ public class CppGenerator {
 	}
 
 	private static void printParametersBindingAll(String topic) {
-		printer.printLn(HeaderGenerator.addedTopics.get(topic).get(0).classifyMsgType() 
+		printer.printLn(HeaderGenerator.addedTopics.get(topic).get(0).classifyMsgType()
 				+ " " + GeneratorUtil.MONITOR_COPY_MSG_NAME + ";");
 
 		Map<String, String> msgMapping = new HashMap<String, String>();
@@ -407,7 +407,7 @@ public class CppGenerator {
 		}
 
 		for (String msgName : msgMapping.keySet()) {
-			printer.printLn(GeneratorUtil.MONITOR_COPY_MSG_NAME + "." + msgName + " = " 
+			printer.printLn(GeneratorUtil.MONITOR_COPY_MSG_NAME + "." + msgName + " = "
 					+ GeneratorUtil.MONITORED_MSG_NAME + "->" + msgName + ";");
 		}
 
@@ -419,18 +419,18 @@ public class CppGenerator {
 				for (Variable parameter : event.getParameters()) {
 					if(noDoubleParam.contains(parameter.getDeclaredName())){
 						if(parameter.getType().endsWith("[]")){
-							printer.print("vector<" + parameter.getType().replace("[]", "") 
+							printer.print("vector<" + parameter.getType().replace("[]", "")
 									+ ">" + "& " + parameter.getDeclaredName() + " = ");
 						} else
-							printer.print(parameter.getType() + "& " 
+							printer.print(parameter.getType() + "& "
 									+ parameter.getDeclaredName() + " = ");
-						printer.print(GeneratorUtil.MONITOR_COPY_MSG_NAME + "." 
+						printer.print(GeneratorUtil.MONITOR_COPY_MSG_NAME + "."
 									+ event.getPattern().get(parameter.getDeclaredName()) + ";");
 						printer.printLn();
 
 						noDoubleParam.remove(parameter.getDeclaredName());
 					} else{
-						System.err.println("Duplicate parameter-pattern matching!: \t" 
+						System.err.println("Duplicate parameter-pattern matching!: \t"
 								+ parameter.getDeclaredName()
 								+ "\n(Beware: Same named parameters are removed.)");
 					}
@@ -452,7 +452,7 @@ public class CppGenerator {
 
 		Map<String, String> msgMapping = MessageParser.parseMessage(event.getMsgType());
 		for (String msgName : msgMapping.keySet()) {
-			printer.printLn(GeneratorUtil.MONITOR_COPY_MSG_NAME + "." + msgName + " = " 
+			printer.printLn(GeneratorUtil.MONITOR_COPY_MSG_NAME + "." + msgName + " = "
 					+ GeneratorUtil.MONITORED_MSG_NAME + "->" + msgName + ";");
 		}
 
@@ -462,12 +462,12 @@ public class CppGenerator {
 		if(event.getParameters() != null){
 			for (Variable parameter : event.getParameters()) {
 				if(parameter.getType().endsWith("[]")){
-					printer.print("vector<" + parameter.getType().replace("[]", "") 
+					printer.print("vector<" + parameter.getType().replace("[]", "")
 							+ ">" + "& " + parameter.getDeclaredName() + " = ");
 				} else
-					printer.print(parameter.getType() + "& " 
+					printer.print(parameter.getType() + "& "
 							+ parameter.getDeclaredName() + " = ");
-				printer.print(GeneratorUtil.MONITOR_COPY_MSG_NAME + "." 
+				printer.print(GeneratorUtil.MONITOR_COPY_MSG_NAME + "."
 							+ event.getPattern().get(parameter.getDeclaredName()) + ";");
 				printer.printLn();
 			}
@@ -521,18 +521,18 @@ public class CppGenerator {
 			printer.printLn();
 			printer.printLn("}");
 		}
-		printer.printLn();		
+		printer.printLn();
 	}
 
 	private static void publishAndSerializeMsg() {
 		// ros::SerializedMessage serializedMsg = ros::serialization::serializeMessage(msgName);
 		// publishPtr->publish(serializedMsg);
-		String serializedMessage = 
-				"ros::SerializedMessage serializedMsg = ros::serialization::serializeMessage(" 
+		String serializedMessage =
+				"ros::SerializedMessage serializedMsg = ros::serialization::serializeMessage("
 				+ GeneratorUtil.MONITOR_COPY_MSG_NAME + ");";
 		printer.print(serializedMessage);
 		printer.printLn();
-		serializedMessage = GeneratorUtil.SERVERMANAGER_PTR_NAME 
+		serializedMessage = GeneratorUtil.SERVERMANAGER_PTR_NAME
 				+ "->publish(" + GeneratorUtil.TOPIC_PTR_NAME + ", serializedMsg);";
 		printer.print(serializedMessage);
 	}
